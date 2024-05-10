062921355836af845a2eaf394fb6a6b15e2a0f69
114094_Di_Bella_Pedro_Agustin <114094@tecnicatura.frc.utn.edu.ar>
2024-05-10 10:52:35 -0300

    Continuo con el codigo de la clase 03-05

--- Clase 03-05/scriptValidaciones.js
+++ Clase 03-05/scriptValidaciones.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
   $("#formLoginEquipos").validate({
    rules: {
        denominacion: {
@@ -24,14 +24,46 @@ $(document).ready(function(){
         minlenght: "la contraseña no puede ser menor a 8 caracteres"
        }
    }
+   
 
   });
-});
-if(){
-  $(document).ready(function(){
-    $("#btnIniciarSesion").click(function(){
-      $(location).attr('href','./SegundaPagina.html')
-    })  
-  })
-  
-}
+});*/
+$("#formLoginEquipos").validate({
+    rules:{
+        denominacion:{
+        required:true,
+        minlenght:1,
+        maxlength: 70
+    },
+    password:{
+      required:true,
+      minlength:8,
+      maxlength:16
+    }
+    
+    },
+    messages:{
+        denominacion:{
+            required:"La DENOMINACION del club es obligatorio"
+        },
+        password: {
+            required: "Necesita una contraseña",
+            minlenght: "la contraseña no puede ser menor a 8 caracteres"
+           }
+    }
+})
+
+$("#formLoginEquipos").submit((e) => {
+  e.preventDefault(); // Prevenir el envío del formulario
+
+  if ($("#formLoginEquipos").valid()) {
+    const denominacion = $("#denominacion").val();
+    const password = $("#password").val();
+
+    // Redireccionar a la nueva página con valores pasados por GET
+    window.location.href = 'https://./SegundaPagina.html?denominacion=${encodeURIComponent(denominacion)}&password=${encodeURIComponent(password)}';
+  }
+})
+
+
+
